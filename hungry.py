4935c33df12b0bb72f247dd38cc745e596ed75e5
harsh11shukla <shukla.harsh1211@gmail.com>
2024-03-11 12:47:47 -0700

    water

--- hungry.py
+++ hungry.py
@@ -5,4 +5,7 @@ if hungry=="yes":
   print("eat pussy")
   print("eat burger")
 else:
-  print("Do your homework")
+  thirsty=input("Are you thirsty : ")
+  if thirsty==yes:
+    print("drink water")
+  
